ba1b7892a6317eb32c71429df034aaef32d61817
DarrickPrichett <darrickprichett@gmail.com>
2021-08-29 12:58:03 -0500

    added ; at the end of codelines

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 var symbols = ['`','~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','[','{',']','}',';','|','<','>','?']
-var allPasswordOptions = []
+var allPasswordOptions = [];
+var password = [];
 function generatePassword () {
 
  // let passwordOptions = generatePasswordOptions() 
@@ -15,12 +16,12 @@ function generatePassword () {
    
   if (resultNumChara < 8) {
     alert('Password must contain at least 8 characters.');
-    console.log("less than 8")
+    console.log("less than 8");
     return;
   };
   if (resultNumChara > 128) {
     alert('Password has a maximum of 128 characters');
-    console.log("greate than 128")
+    console.log("greater than 128");
     return;
   };
 
@@ -31,30 +32,31 @@ function generatePassword () {
     return;
   };
   if(resultNumChara == 'numbers'){
-    resultNumChara = passLenth;
+    resultNumChara = resultNumChara;
+    console.log(resultNumChara);
   }
 
   let resultUpper = confirm('Would you like to use Uppercase letters?');
   if (resultUpper) {
-    allPasswordOptions = allPasswordOptions.concat(upper)
+    allPasswordOptions = allPasswordOptions.concat(upper);
     console.log(resultUpper);
   }
 
   let resultLower = confirm('Would you like to use Lowercase letters?');
   if (resultLower) {
-    allPasswordOptions = allPasswordOptions.concat(lower)
+    allPasswordOptions = allPasswordOptions.concat(lower);
     console.log(resultLower);
   }
 
   let resultSymbols = confirm('Would you like to use Symbols?');
   if (resultSymbols) {
-    allPasswordOptions = allPasswordOptions.concat(symbols)
+    allPasswordOptions = allPasswordOptions.concat(symbols);
     console.log(resultSymbols);
   }
 
   let resultNumbers = confirm('Would you like to use numbers?');
   if (resultNumbers) {
-    allPasswordOptions = allPasswordOptions.concat(numbers)
+    allPasswordOptions = allPasswordOptions.concat(numbers);
     console.log(resultNumbers);
     console.log(allPasswordOptions);
   }
